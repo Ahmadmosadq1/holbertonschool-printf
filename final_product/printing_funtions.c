3e96008efb92557316dd7fbfae606bada78c4dba
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-25 19:13:58 +0000

    _printf.c

---
+++ final_product/printing_funtions.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_string - Entry point
+ * Description: This function prints a string for %s format.
+ * @format: data type
+ * Return: J
+ */
+int print_string(char *format)
+{
+	int j = 0;
+
+	if (format == NULL)
+		format = "(null)";
+	while (format[j] != '\0')
+	{
+		j++;
+	}
+	write(1, format, j);
+	return (j);
+}
+/**
+ * print_char - Entry point
+ *
+ * Description: This function prints a charachter for %c format.
+ * @c: charachter.
+ * @i: increment i to skip %c.
+ * Return: J
+ */
+int print_char(char c, int *i)
+{
+	write(1, &c, 1);
+	*i += 2;
+	return (1);
+}
+
