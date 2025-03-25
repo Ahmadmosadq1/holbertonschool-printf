801bbc50289781aa2cb25bb2c5b01bc49b54ef3d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-25 14:35:35 +0000

    _printf.c

--- test/_printf.c
+++ test/_printf.c
@@ -6,50 +6,58 @@
 
 int _printf(const char *format, ...)
 {
-  int i = 0;
-    char *str;
-    va_list arg;
-    int printed = 0;/*to start calculating how many chars printed*/
+	int i = 0;
+	char *str;
+	va_list arg;
+	int printed = 0;
 
-    if (format == NULL)/*checking if format is NULL and terminates if so*/
-    {
-        write(1, "failed", 6);
-	printed++;
-        return (0);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(arg, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			char c;
+
+			c = va_arg(arg, int);
+			write(1, &c, 1);
+			printed++;
+			i += 2;
+			continue;
+		}
+
+		if (format[i] == '%' && format[i + 1] == 's')
+		{
+			str = va_arg(arg, char *);
+
+			if (str == NULL)
+				str = "(null)";
+			printed = printed + print_string(str);
+			i += 2;
+			continue;
+		}
+
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(1, &format[i], 1);
+			printed++;
+			i += 2;
+			continue;
+		}
+
+		if (format[i] == '%')
+			return(0);
+		else
+		{write(1, &format[i], 1);
+			printed++;
+		}
+        	i++;
     }
-    va_start(arg, format);/*initilizing the varadiac funtion.*/
-    while (format[i]) /*looping through the format string.*/
-    {
-        if (format[i] == '%' && format[i + 1] == 'c')/*if % is found and c after it, we can print the char*/
-        {
-            char c = va_arg(arg, int); /*assigning ecah arg to c.*/
-            write(1, &c, 1 ); /*printing out each charachter*/
-	    printed++;
-            i = i + 2 ;/*to skip the (%c) in thr formatt so we dont print them and look for thr next(%)*/
-            continue;
-        }
-        if (format[i] == '%' && format[i + 1] == 's')/* if % is follwed by a S, we will print a string*/
-        {
-            str = va_arg(arg, char *);
-	    if (str == NULL)
-		    str = "(null)";
-            printed = printed + print_string(str);/*printing string funtion.*/
-            i = i + 2;
-            continue;
-        }
-
-	 if(format[i] == '%' && format[i+1] =='%') /* check for '%%'*/
-        {
-                write(1, '%', 1); /* write '%' to stdout*/
-                printed++; /*increment the printed character counter */
-                i+=2; /*skip '%%' int the format strind*/
-                continue; /*move to the next character*/
-        }
-        write(1, &format[i], 1); /*print regular characters*/
-        printed++;
-        i++;
-    }
-
-   va_end(arg);/* clean up the argumnt list*/
-   return(printed);
+	va_end(arg);
+   	return(printed);
 }
