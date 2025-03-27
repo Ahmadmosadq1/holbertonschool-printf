c58319cf04516fefe997a9e52b991f133fa47104
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-03-27 01:05:22 +0000

    3_printf.c

--- test_A/main.c
+++ test_A/main.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 int  main(void)
 {
-	_printf("%s", "string");
-	_printf("%c", 'c');
+	_printf("%s\n", "string");
+	_printf("%c\n", 'c');
 	 _printf("%");
-	 _printf("Integer: %d\n", 1234);
+	 _printf("Integer: %d and %i\n", 1234, -1996);
     _printf("Negative Integer: %i\n", -5678);
+    _printf("%b\n", 98);
 
 	return (0);
 }
