1aac0e26230d2b3f46c19dc03a9b557029b4e1d6
SalehFulayyih <saleh.5050@hotmail.com>
2025-03-28 15:41:03 +0000

    edite some file

---
+++ print_task7.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_nonPprintable - Prints a string with non-printable characters.Non-printable characters (0 < ASCII value < 32 or >= 127)
+ * @str: The string to print.
+ * @i: Pointer to index in format string.
+ * Return: Number of characters printed.
+ */
+
+int print_nonPrintable(char *str, int *i)
+{
+	int count = 0;
+	char hexa[5]; /* to store \x + tow hexa + \0*/
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sprintf(hexa, "\\X%02X", (unsigned char)*str);
+			write(1, hexa, 4);
+			count++;
+		}
+		else
+		{
+			write(1, str, 1);
+			count++;
+		}
+		str++;
+	}
+	return (count);
+}
